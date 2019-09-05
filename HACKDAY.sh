7e05d80aa8ff87cfacb1449936c635c5ae4ca8cc
hackday404 <54924067+hackday404@users.noreply.github.com>
2019-09-05 10:29:49 +0700

    Add files via upload

---
+++ HACKDAY.sh
@@ -0,0 +1,168 @@
+#!/usr/bin/bash
+#Tools SadapWhatsApp
+#Sadap WhatsApp2019
+#By Mr.Kaneki
+#Jangan DiRecord Tod!!!
+#Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $blue "<|            Tools SadapWhatsApp       |>"
+echo  $blue "<|                                      |>"
+echo  $blue "<| Name: MrKaneki    ||    MrKaneki   |>"
+echo  $blue "<| MyTeam: CYBER NET MUSLIM             |>"
+echo  $blue "<| Contact: private        |>"
+echo  $blue "<| Thanks To: CNM CYBER TEAM            |>"
+echo  $blue "<|======================================|>"
+sleep 1
+echo  $red "============== $red MENU $green ================="
+echo $blue "================================================="
+echo $blue "|| $green 1. Sadap WhatsApp No Indosat   $blue ||"
+echo $blue "|| $green 2. Sadap WhatsApp No Telkomsel $blue ||"
+echo $blue "|| $green 3. Sadap WhatsApp No XL / Axis $blue ||"
+echo $blue "|| $green 4. Sadap WhatsApp No 3(Three)  $blue ||"
+echo $blue "|| $green 0. Exit Sadap WhatsApp         $blue ||"
+echo $blue "================================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@MrKaneki=•>" MrKaneki;
+if [ $MrKaneki = 1 ] || [ $MrKaneki = 01 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@Kaneki=•>" MR.kaneki;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 3
+echo $blue "10%"
+sleep 4
+echo $green "20%"
+sleep 2
+echo $cyan "30%"
+sleep 8
+echo $purple "40%"
+sleep 3
+echo $blue "50%"
+sleep 2
+echo $red "60%"
+sleep 5
+echo $green "70%"
+sleep 6
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 3
+echo $blue "100%"
+sleep 2
+echo $green "Prosessing Done"
+sleep 4
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Verifikasi"fi
+fi
+
+if [ $MrKaneki = 2 ] || [ $MrHACKDAY = 02 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@MrHACKDAY=•>" MrHACKDAY;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 5
+echo $blue "10%"
+sleep 3
+echo $green "20%"
+sleep 8
+echo $cyan "30%"
+sleep 3
+echo $purple "40%"
+sleep 6
+echo $blue "50%"
+sleep 5
+echo $red "60%"
+sleep 4
+echo $green "70%"
+sleep 6
+echo $cyan "80%"
+sleep 8
+echo $purple "90%"
+sleep 6
+echo $blue "100%"
+sleep 9
+echo $green "Prosessing Done"
+sleep 3
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap silahkan cek sdcard kalian WhatsappDATABASE.TXT  $Verifikasi"
+fi
+
+if [ $MrHACKDAY = 3 ] || [ $MrHACKDAY = 03 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@MrHACKDAY=•>" MrHACKDAY;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 8
+echo $blue "10%"
+sleep 4
+echo $green "20%"
+sleep 5
+echo $cyan "30%"
+sleep 6
+echo $purple "40%"
+sleep 4
+echo $blue "50%"
+sleep 2
+echo $red "60%"
+sleep 3
+echo $green "70%"
+sleep 9
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 4
+echo $blue "100%"
+sleep 9
+echo $green "Prosessing Done"
+sleep 9
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Nomer $Kode Verifikasi"
+fi
+
+if [ $MrHACKDAY = 4 ] || [ $MrHACKDAY = 04 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@MrHACKDAY=•>" MrHACKDAY;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 8
+echo $blue "10%"
+sleep 7
+echo $green "20%"
+sleep 5
+echo $cyan "30%"
+sleep 4
+echo $purple "40%"
+sleep 5.5
+echo $blue "50%"
+sleep 4
+echo $red "60%"
+sleep 5
+echo $green "70%"
+sleep 6
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 5
+echo $blue "100%"
+sleep 3
+echo $green "Prosessing Done"
+sleep 4
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap silahkan cek sdcard kalian format "whatsappdatabase.txt" $Nomer $Kode Verifikasi"
+fi
+
+if [ $MrHACKDAY = 0 ] || [ $MrHACKDAY = 00 ]
+then
+echo  $red "Bye Bye Sayang"
+echo  $blue "Jangan Lupa HARGAI KARYA ORG LAIN SBLM ANDA BACOT!"
+echo  $green "Mr.HACKDAY Cyber Team!"
+exit
+fi
+
